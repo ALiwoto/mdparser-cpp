73407100ea3218d6f4f9a0cd1b33b7b9cb6906fe
Ammar Faizi <ammarfaizi2@gnuweeb.org>
2021-10-17 21:37:51 +0700

    Add a unit test for static_helpers in WotoMd.h
    To make sure static inline functions in WotoMd.h are working properly,
    create a test for them. This is also the initial work for creating
    other tests in directory `tests/`.
    
    Signed-off-by: Ammar Faizi <ammarfaizi2@gnuweeb.org>

---
+++ tests/static_helpers.cpp
@@ -0,0 +1,22 @@
+/* SPDX-License-Identifier: MIT */
+
+#include <cstdio>
+#include <cassert>
+#include <WotoMd.h>
+
+int main(void)
+{
+	assert(toBold("AAAAAA") == "*AAAAAA*");
+	assert(toBold("*A*A*A*A*A*A") == "*\\*A\\*A\\*A\\*A\\*A\\*A*");
+
+	assert(toBold("AAAAAA", true) == "*AAAAAA* ");
+	assert(toBold("*A*A*A*A*A*A", true) == "*\\*A\\*A\\*A\\*A\\*A\\*A* ");
+
+	assert(toUnderline("AAAAAA") == "-AAAAAA-");
+	assert(toUnderline("-A-A-A-A-A-A") == "-\\-A\\-A\\-A\\-A\\-A\\-A-");
+
+	assert(toUnderline("AAAAAA", true) == "-AAAAAA- ");
+	assert(toUnderline("-A-A-A-A-A-A") == "-\\-A\\-A\\-A\\-A\\-A\\-A- ");
+
+	return 0;
+}
